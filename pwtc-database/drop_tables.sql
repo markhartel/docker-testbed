e468d1ba7a8a2220820a2c04bee6adc46358c637
Mark Hartel <mark_hartel@hotmail.com>
2016-10-22 19:28:43 -0700

    Add sql to drop views and tables

---
+++ pwtc-database/drop_tables.sql
@@ -0,0 +1,3 @@
+drop view if exists pwtc_ly_rides_vw, pwtc_ytd_rides_vw, pwtc_ly_led_vw, pwtc_ytd_led_vw, pwtc_ly_rides_led_vw, pwtc_ytd_rides_led_vw, pwtc_ly_lt_achvmnt_vw, pwtc_ybl_lt_miles_vw, pwtc_ly_lt_miles_vw, pwtc_ly_miles_vw, pwtc_ytd_miles_vw, pwtc_lt_miles_vw;
+
+drop table if exists pwtc_ride_leaders, pwtc_ride_mileage, pwtc_club_rides, pwtc_membership;
